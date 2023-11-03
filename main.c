76a291d97c475d79316a8f98ea0cbdf69373e7dc
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-11-03 18:11:42 +0200

    basic user line input testing

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_line - reads the input string.
+ *
+ * @i_eof: return value of getline function
+ * Return: input string
+ */
+char *read_user_input(int *gt_line)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*gt_line = getline(&input, &bufsize, stdin);
+
+	return (input);
+}
+
+
+/**
+ * main - Simple Shell
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int gt_line;
+	char *input;
+
+	while (1) {
+
+		write(STDIN_FILENO, "$ ", 4);
+		input = read_user_input(&gt_line);
+
+		if (gt_line != -1)
+		{
+			printf("%s", input);
+		}
+
+	}
+}
+
